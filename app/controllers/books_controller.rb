c5281943438241b08126417baf8c275e126d689c
EC2 Default User <ec2-user@ip-172-31-80-140.ec2.internal>
2020-09-16 08:32:21 +0000

    課題２

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,29 +17,32 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
-  end
-
-  def destroy
-    book = Book.find(params[:id])  # データ（レコード）を1件取得
-    book.destroy  # データ（レコード）を削除
-    redirect_to books_path  # 投稿一覧画面へリダイレクト
-  end
-
-  def create
-    # １. データを新規登録するためのインスタンス作成
-    @book = Book.new(book_params)
-    # ２. データをデータベースに保存するためのsaveメソッド実行
-    if @book.save
-    # ３. トップ画面へリダイレクト
-        # redirect_to '/top' を削除して、以下コードに変更
-    # 詳細画面へリダイレクト
+    @book = Book.find(params[:id])
+    if  @book.update(book_params)
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
+      render :edit
+
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  def create
+
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      # @book = Book.new
       render :index
       # redirect_to books_path
     end
@@ -53,4 +56,5 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
 end
